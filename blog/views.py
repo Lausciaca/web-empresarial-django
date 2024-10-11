9232e1b23abd15b63d9285f0ff23c355c43c14bd
Laureano <laureanosciacaluga@gmail.com>
2024-10-11 10:58:11 -0300

    Configurar vistas blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Category, Post
 
 # Create your views here.
+def blog(request):
+    return render(request, 'blog/blog.html', {
+        'categories': Category.objects.all,
+        'posts': Post.objects.all
+    })
